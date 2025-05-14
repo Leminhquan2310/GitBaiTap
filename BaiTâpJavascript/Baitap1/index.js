19697bc2ca1c1f7b8123b89eb72e9caa00469737
minhquan <quanleminh2310@gmail.com>
2025-05-14 14:43:12 +0700

    add Baitap1 foleder

---
+++ BaiTâpJavascript/Baitap1/index.js
@@ -0,0 +1,15 @@
+// function showMessage() {
+//   alert("Xin Chào!");
+// }
+
+// function showMessage() {
+//   document.getElementById("messageDiv").innerHTML = "Xin Chào";
+// }
+
+function showMessage() {
+  document.write("Xin chào");
+}
+
+//  function showMessage(){
+//      console.log('Xin chào!');
+//  }
